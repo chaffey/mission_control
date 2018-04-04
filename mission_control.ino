d21f3b8a4842e4c200b5d677e4d0ae2367a8aa46
Chris Haffey <chris.haffey@clickbank.com>
2018-04-04 10:25:53 -0600

    More udpates to hook up buttons/switches to lights.  Also added the serial message system to the rpi

--- mission_control.ino
+++ mission_control.ino
@@ -11,11 +11,7 @@
 const PROGMEM uint8_t BACKPACK_COUNT = 3;
 const PROGMEM uint8_t EXPANDER_COUNT = 5;
 
-const PROGMEM uint8_t BRIGHTNESS_0 = 7;
-const PROGMEM uint8_t BRIGHTNESS_1 = 7;
-const PROGMEM uint8_t BRIGHTNESS_2 = 7;
-
-const byte SERIAL_DELIMITER = '|';
+const char SERIAL_DELIMITER = '|';
 
 char buf[17];
 
@@ -25,7 +21,7 @@ Adafruit_8x16matrix backpacks[BACKPACK_COUNT] = {
   Adafruit_8x16matrix()
 };
 
-// 4 x 8 (cathodes w/ 16 bit anode value)
+// 3 x 8 (cathodes w/ 16 bit anode value)
 uint16_t led_buffers [BACKPACK_COUNT][8] = {
   {
     0b0000000000000000,
@@ -78,28 +74,42 @@ unsigned long messageTime;
 unsigned long now;
 
 bool flushing = false;
+bool launchActive = false;
 uint8_t flushCount = 0;
 unsigned long flushTime;
+float dim = 7;
+float dimDir = 0;
 
-boolean s_standby = true;
-boolean s_uplink = false;
-boolean s_thrust = false;
-boolean s_mainChute = false;
-boolean s_mechFailure = false;
-boolean s_hatch = false;
-boolean s_health = false;
-boolean s_eject = false;
-boolean s_wasteSystem = false;
-boolean s_battery = false;
-boolean s_cabinTemp = false;
-boolean s_engineFault = false;
-boolean s_o2Level = false;
-boolean s_h2Level = false;
-boolean s_o2Press = false;
-boolean s_h2Press = false;
-boolean s_fire = false;
+bool s_standby = true;
+bool s_uplink = false;
+bool s_thrust = false;
+bool s_mainChute = false;
+bool s_mechFailure = false;
+bool s_hatch = false;
+bool s_health = false;
+bool s_eject = false;
+bool s_wasteSystem = false;
+bool s_battery = false;
+bool s_cabinTemp = false;
+bool s_engineFault = false;
+bool s_o2Level = false;
+bool s_h2Level = false;
+bool s_o2Press = false;
+bool s_h2Press = false;
+bool s_fire = false;
 unsigned long fireSuppressOffTime;
-boolean s_temp = false;
+bool s_temp = false;
+
+unsigned long mealPrepCutoff;
+unsigned long compactorCutoff;
+unsigned long suit1Cutoff;
+unsigned long suit2Cutoff;
+unsigned long healthCutoff;
+uint8_t healthVal;
+
+bool thrustState[3] = { 0, 0, 0 };
+bool rcsState[3] = { 0, 0, 0 };
+unsigned long musicChangeCutoff;
 
 int iteration = 0;
 
@@ -125,21 +135,21 @@ void setup() {
   expanders[4].begin(3);
 
   backpacks[0].begin(0x70);
-  backpacks[0].setBrightness(BRIGHTNESS_0);
+  backpacks[0].setBrightness(dim);
 
   backpacks[1].begin(0x71);
-  backpacks[1].setBrightness(BRIGHTNESS_1);
+  backpacks[1].setBrightness(dim);
 
   backpacks[2].begin(0x72);
-  backpacks[2].setBrightness(BRIGHTNESS_2);
+  backpacks[2].setBrightness(dim);
 
-  for (int i = 0; i < BACKPACK_COUNT; ++i) {
-    backpackOn(i);
-    displayMatrix(i);
-    delay(250);
-    backpackOff(i);
-    displayMatrix(i);
-  }
+  //  for (int i = 0; i < BACKPACK_COUNT; ++i) {
+  //    backpackOn(i);
+  //    displayMatrix(i);
+  //    delay(1000);
+  //    backpackOff(i);
+  //    displayMatrix(i);
+  //  }
 
   for (int i = 0; i < EXPANDER_COUNT; ++i) {
     for (int j = 0; j < 16; ++j) {
@@ -148,7 +158,7 @@ void setup() {
     }
   }
 
-  delay(100);
+  delay(20);
 
 #ifdef DEBUG
   Serial.println("Setup complete");
@@ -160,7 +170,7 @@ void loop() {
 
   pullSwitchValues();
 
-  if (iteration >= 1000) {
+  if (iteration >= 100) {
     iteration = 0;
 
 #ifdef DEBUG
@@ -184,6 +194,8 @@ void loop() {
   }
 
   iteration++;
+
+  handleMessageFromPi();
 }
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
@@ -213,45 +225,173 @@ void processCrewSafetyPanel() {
       fireSuppressOffTime = 0;
     }
   }
+
+  setLed(0, 1, 13, switchState(3, 2));
+  setLed(0, 1, 14, isBlinkOn(123) && switchState(3, 3));
+
+  if (switchState(3, 5) && switchState(3, 4)) {
+    dim = dim + dimDir;
+    if (dim < 1) {
+      dim = 1;
+    } else if (dim > 7) {
+      dim = 7;
+    }
+    for (int i = 0; i < 3; ++i) {
+      backpacks[i].setBrightness(dim);
+    }
+  } else if (switchState(3, 5) && !switchState(3, 4)) {
+    dimDir = -0.2;
+  } else if (!switchState(3, 5) && switchState(3, 4)) {
+    dimDir = 0.2;
+  }
 }
 
 void processLaunchControlPanel() {
+  if (!launchActive && switchState(1, 8)) {
+    launchActive = true;
+    messagePi("LAUNCH");
+  }
+  if (launchActive && switchState(1, 9)) {
+    launchActive = false;
+    messagePi("ABORT");
+  }
+  setLed(0, 1, 4, launchActive);
+  setLed(0, 1, 5, !launchActive || isBlinkOn(127));
 }
 
 void processCrewLifePanel() {
-  if (!musicEnabled && switchState(2, 7) == HIGH) {
+  if (!musicEnabled && switchState(2, 7)) {
     musicEnabled = true;
     messagePi("MUSIC:ON");
-  } else if (musicEnabled && switchState(2, 7) == LOW) {
+  } else if (musicEnabled && !switchState(2, 7)) {
     musicEnabled = false;
     messagePi("MUSIC:OFF");
   }
 
+  if (musicEnabled && switchState(2, 6) && musicChangeCutoff < now) {
+    messagePi("MUSIC:CHANGE");
+    musicChangeCutoff = now + 3000;
+  }
+
   // music light
-  setLed(0, 1, 9, isBlinkOn(2000) && musicEnabled);
+  setLed(0, 1, 9, musicEnabled);
+
+  // unsigned long mealPrepCutoff;
+  // unsigned long compactorCutoff;
+
+  if (switchState(2, 0) && suit2Cutoff == 0) {
+    suit2Cutoff = now + 30000;
+  } else if (suit2Cutoff < now) {
+    suit2Cutoff = 0;
+  }
+
+  if (switchState(2, 1) && compactorCutoff == 0) {
+    compactorCutoff = now + 30000;
+  } else if (compactorCutoff < now) {
+    compactorCutoff = 0;
+  }
+
+  if (switchState(2, 2) && suit1Cutoff == 0) {
+    suit1Cutoff = now + 30000;
+  } else if (suit1Cutoff < now) {
+    suit1Cutoff = 0;
+  }
+
+  if (switchState(2, 3) && mealPrepCutoff == 0) {
+    mealPrepCutoff = now + 30000;
+  } else if (mealPrepCutoff < now) {
+    mealPrepCutoff = 0;
+  }
+
+  if (switchState(2, 4) && healthCutoff == 0) {
+    healthCutoff = now + 30000;
+    healthVal = random(1, 5);
+  } else if (healthCutoff < now) {
+    healthCutoff = 0;
+  }
+
+  for (int i = 0; i < 4; ++i) {
+    setLed(0, 3, 3 - i, i == healthVal - 1);
+  }
+
+  setLed(0, 3, 4, mealPrepCutoff != 0);
+  setLed(0, 3, 5, compactorCutoff != 0);
+  setLed(0, 3, 6, suit1Cutoff != 0);
+  setLed(0, 3, 7, suit2Cutoff != 0);
+
+  if (!flushing && switchState(2, 5)) {
+    flushing = true;
+    messagePi("FLUSH");
+  }
 }
 
 void processPyrotechnicsPanel() {
+  if (switchState(2, 10) && !thrustState[0]) {
+    thrustState[0] = true;
+    messagePi("THRUST_RIGHT:ON");
+  } else if (!switchState(2, 10) && thrustState[0]) {
+    thrustState[0] = false;
+    messagePi("THRUST_RIGHT:OFF");
+  }
+  if (switchState(2, 9) && !thrustState[1]) {
+    thrustState[1] = true;
+    messagePi("THRUST_CENTER:ON");
+  } else if (!switchState(2, 9) && thrustState[1]) {
+    thrustState[1] = false;
+    messagePi("THRUST_CENTER:OFF");
+  }
+  if (switchState(2, 8) && !thrustState[2]) {
+    thrustState[2] = true;
+    messagePi("THRUST_LEFT:ON");
+  } else if (!switchState(2, 8) && thrustState[2]) {
+    thrustState[2] = false;
+    messagePi("THRUST_LEFT:OFF");
+  }
+
+  if (switchState(2, 13) && !rcsState[0]) {
+    rcsState[0] = true;
+    messagePi("RCS_RIGHT:ON");
+  } else if (!switchState(2, 13) && rcsState[0]) {
+    rcsState[0] = false;
+    messagePi("RCS_RIGHT:OFF");
+  }
+  if (switchState(2, 12) && !rcsState[1]) {
+    rcsState[1] = true;
+    messagePi("RCS_CENTER:ON");
+  } else if (!switchState(2, 12) && rcsState[1]) {
+    rcsState[1] = false;
+    messagePi("RCS_CENTER:OFF");
+  }
+  if (switchState(2, 11) && !rcsState[2]) {
+    rcsState[2] = true;
+    messagePi("RCS_LEFT:ON");
+  } else if (!switchState(2, 11) && rcsState[2]) {
+    rcsState[2] = false;
+    messagePi("RCS_LEFT:OFF");
+  }
 }
 
 void processCryogenicsPanel() {
   // o2 fan
-  setLed(0, 4, 1, switchState(3, 8));
+  setLed(0, 0, 0, switchState(3, 8));
   // h2 fan
-  setLed(0, 4, 2, switchState(3, 9));
+  setLed(0, 0, 2, switchState(3, 9));
   // heater
-  setLed(0, 4, 3, switchState(3, 10));
+  setLed(0, 0, 3, switchState(3, 10));
   // o2 pump
-  setLed(0, 4, 4, switchState(3, 11));
+  setLed(0, 0, 4, switchState(3, 11));
   // h2 pump
-  setLed(0, 4, 5, switchState(3, 12));
+  setLed(0, 0, 5, switchState(3, 12));
 }
 
 void processSystemControlPanel() {
   for (int i = 0; i < 8; ++i) {
+    // 9 - 16
     setLed(0, 2, i, switchState(1, i));
+    // 1 - 8
     setLed(0, 3, 8 + i, switchState(0, i));
-    setLed(0, 2, 15 - i, switchState(1, 8 + i));
+    // 17 - 24
+    setLed(0, 2, 15 - i, switchState(0, 8 + i));
   }
 }
 
@@ -266,7 +406,7 @@ void processCommunicationsPanel() {
   }
 
   if (messageTime < now) {
-    if (switchState(2, 15) == HIGH) {
+    if (switchState(2, 14) == HIGH) {
       if (!messageActive) {
         messagePi("MESSAGE");
         messageActive = true;
@@ -274,7 +414,7 @@ void processCommunicationsPanel() {
 
       messageTime = now + random(60000, 180000);
     } else {
-      setLed(0, 4, 0, isBlinkOn(2000));
+      setLed(0, 0, 0, isBlinkOn(500));
     }
   }
 
@@ -403,6 +543,7 @@ void intToBinaryCharArray(uint16_t val, char* o_buffer) {
 }
 
 void debugInputs() {
+  Serial.println();
   Serial.print("Inputs: ");
   for (uint16_t i = 0; i < EXPANDER_COUNT; ++i) {
     Serial.print(i);
@@ -430,8 +571,20 @@ void debugOutputs() {
 }
 
 void messagePi(const char* message) {
-  Serial.print(SERIAL_DELIMITER);
-  Serial.print(message);
-  Serial.print(SERIAL_DELIMITER);
+  Serial.println(message);
+}
+
+void handleMessageFromPi() {
+  int x;
+  String str;
+
+  if (Serial.available() > 0) {
+    str = Serial.readStringUntil('\n');
+    if (str.equalsIgnoreCase("FLUSH_COMPLETE")) {
+      flushing = false;
+    } else if (str.equalsIgnoreCase("FIRE")) {
+      s_fire = true;
+    }
+  }
 }
 
